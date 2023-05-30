caec498b9319e9a985c3fb88bb504ddfc3a93eea
Jon <jwaynelms@gmail.com>
2023-05-30 15:04:26 -0400

    completed average_height.py

---
+++ average_height.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+
+def avg_height():
+    student_heights = input("Input a list of student heights ").split()
+    for n in range(0, len(student_heights)):
+        student_heights[n] = int(student_heights[n])
+    h_total = 0
+    s_total = 0
+    for a in student_heights:
+        h_total += a
+        s_total += student_heights.count(a)
+    avg = h_total / s_total
+    print(h_total)
+    print(s_total)
+    print(student_heights)
+    print(round(avg))
+
+
+avg_height()
