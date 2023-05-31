e67cea1af2e7972f67e3981f845c9dc9e37a0bd9
Jon <jwaynelms@gmail.com>
2023-05-31 16:38:01 -0400

    finished highest_score.py

--- average_height.py
+++ average_height.py
@@ -4,16 +4,30 @@ def avg_height():
     student_heights = input("Input a list of student heights ").split()
     for n in range(0, len(student_heights)):
         student_heights[n] = int(student_heights[n])
-    h_total = 0
-    s_total = 0
-    for a in student_heights:
-        h_total += a
-        s_total += student_heights.count(a)
-    avg = h_total / s_total
-    print(h_total)
-    print(s_total)
-    print(student_heights)
+    print(sum(student_heights))
+    print(len(student_heights))
+    avg = sum(student_heights) / len(student_heights)
     print(round(avg))
-
-
 avg_height()
+    
+avg_height()
+
+
+# below code uses for loops
+# def avg_height():
+#     student_heights = input("Input a list of student heights ").split()
+#     for n in range(0, len(student_heights)):
+#         student_heights[n] = int(student_heights[n])
+#     h_total = 0
+#     s_total = 0
+#     for a in student_heights:
+#         h_total += a
+#         s_total += student_heights.count(a)
+#     avg = h_total / s_total
+#     print(h_total)
+#     print(s_total)
+#     print(student_heights)
+#     print(round(avg))
+
+
+# avg_height()
---
+++ highest_score.py
@@ -0,0 +1,17 @@
+# this calculates the highest scores among inputs from users
+
+def highest_score():
+    student_scores = input("Input a list of student scores ").split()
+    for n in range(0, len(student_scores)):
+        student_scores[n] = int(student_scores[n])
+    print(student_scores)
+
+    highest = student_scores[0]  # Initialize the highest score with the first element
+
+    for score in student_scores:
+        if score > highest:
+            highest = score
+
+    print(f"The highest score is: {highest}")
+
+highest_score()
